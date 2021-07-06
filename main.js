4ec2dc8d18ab8a74e6a034d58769bd7ffcdfb1fb
Roman <dadfrontend@gmail.com>
2021-07-06 23:14:43 +0300

    one commit

---
+++ main.js
@@ -0,0 +1,52 @@
+"use strict";
+// let number = 4.4 / 0;
+
+// console.log(number);
+// console.log('string' * 3);
+
+// const persona = "3";
+// console.log(personal);
+
+// const bool = true;
+
+// const obj = {
+//   name: "Alex",
+//   age: 4
+// }
+ 
+// console.log(obj['name']);
+
+// const pers = ['Alex', 4, 'Pers'];
+// console.log(pers[1]);
+
+// const result = confirm('Ты тут?');
+
+// console.log(result);
+
+// const answers = [];
+
+// answers[0] = prompt('Сколько тебе лет?', '');
+// answers[1] = prompt('Как тебя зовут?', '');
+// answers[2] = prompt('Как твоя фамилия?', '');
+
+// document.write(answers);
+
+// const category = 'toys';
+
+// console.log(`http://same.com/${category}/4`);
+
+// console.log('arr' + ' - это object');
+
+// let incr = 10, 
+//     decr = 10;
+
+//     ++incr;
+//     console.log(incr);
+
+//     --decr;
+//     console.log(7%5);
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
